debd6f72fddb034e57a978188d19e28a3eb70963
sluggets <tim.longoria@gmail.com>
2015-05-06 09:50:11 -0500

    Home page displays word and real def mixed in with fake defs

--- wordfib/views.py
+++ wordfib/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from wordfib.models import WordAndTrue, FakeDefinitions
+from random import randint, shuffle
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    count = WordAndTrue.objects.count()
+    rand_num = randint(1, count);
+    rand_word = WordAndTrue.objects.get(pk=rand_num)
+    def_list = []
+    for defs in (rand_word.fakedefinitions_set.all()):
+        def_list.append(defs.fake_def())
+
+    def_list.append(rand_word.real_definition)
+    shuffle(def_list)
+    
+    return render(request, 'home.html', {'rand_word': rand_word, 'def_list': def_list})
